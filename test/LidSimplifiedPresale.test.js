fc356784aec97687e8fa02ed1add0847591f94cd
Onuwa Nnachi Isaacmatrix4u2002@gmail.com <matrix4u2002@gmail.com>
2020-08-25 06:53:45 +0100

    added tests

--- test/LidSimplifiedPresale.test.js
+++ test/LidSimplifiedPresale.test.js
@@ -1,35 +1,50 @@
-const { accounts, contract, web3 } = require("@openzeppelin/test-environment")
-const { expectRevert, time, BN, ether, balance } = require("@openzeppelin/test-helpers")
-const {expect} = require("chai")
-const config = require("../config")
+const { accounts, contract, web3 } = require("@openzeppelin/test-environment");
+const {
+  expectRevert,
+  time,
+  BN,
+  ether,
+  balance,
+} = require("@openzeppelin/test-helpers");
+const { expect } = require("chai");
+const config = require("../config");
 
-const Token = contract.fromArtifact("Token")
-const TeamLock = contract.fromArtifact("LidTimeLock")
-const DaoLock = contract.fromArtifact("LidTimeLock")
-const LidSimplifiedPresale = contract.fromArtifact("LidSimplifiedPresale")
-const LidSimplifiedPresaleRedeemer = contract.fromArtifact("LidSimplifiedPresaleRedeemer")
-const LidSimplifiedPresaleTimer = contract.fromArtifact("LidSimplifiedPresaleTimer")
+const Token = contract.fromArtifact("Token");
+const TeamLock = contract.fromArtifact("LidTimeLock");
+const DaoLock = contract.fromArtifact("LidTimeLock");
+const LidSimplifiedPresale = contract.fromArtifact("LidSimplifiedPresale");
+const LidSimplifiedPresaleRedeemer = contract.fromArtifact(
+  "LidSimplifiedPresaleRedeemer"
+);
+const LidSimplifiedPresaleTimer = contract.fromArtifact(
+  "LidSimplifiedPresaleTimer"
+);
 
-
-const owner = accounts[0]
-const depositors = [accounts[1],accounts[2],accounts[3],accounts[4],accounts[5]]
-const projectFund = accounts[6]
-const teamFund = accounts[7]
-const initialTokenHolder = accounts[8]
+const owner = accounts[0];
+const depositors = [
+  accounts[1],
+  accounts[2],
+  accounts[3],
+  accounts[4],
+  accounts[5],
+];
+const projectFund = accounts[6];
+const teamFund = accounts[7];
+const initialTokenHolder = accounts[8];
 
 const TOTAL_TOKENS = ether("100000000");
-const SECONDS_PER_HOUR = 3600
+const SECONDS_PER_HOUR = 3600;
 
-describe("LidSimplifiedPresale", function() {
-  before(async function() {
-    this.Token = await Token.new()
-    this.TeamLock = await TeamLock.new()
-    this.DaoLock = await DaoLock.new()
-    this.Presale = await LidSimplifiedPresale.new()
-    this.Redeemer = await LidSimplifiedPresaleRedeemer.new()
-    this.Timer = await LidSimplifiedPresaleTimer.new()
+describe("LidSimplifiedPresale", function () {
+  before(async function () {
+    this.Token = await Token.new();
+    this.TeamLock = await TeamLock.new();
+    this.DaoLock = await DaoLock.new();
+    this.Presale = await LidSimplifiedPresale.new();
+    this.Redeemer = await LidSimplifiedPresaleRedeemer.new();
+    this.Timer = await LidSimplifiedPresaleTimer.new();
 
-    await this.Token.initialize(TOTAL_TOKENS,initialTokenHolder);
+    await this.Token.initialize(TOTAL_TOKENS, initialTokenHolder);
     await this.Redeemer.initialize(
       config.redeemer.redeemBP,
       config.redeemer.redeemInterval,
@@ -57,34 +72,102 @@ describe("LidSimplifiedPresale", function() {
       this.Redeemer.address,
       config.presale.token,
       config.presale.uniswapRouter,
-      config.presale.lidFund,
+      config.presale.lidFund
     );
-    await this.Token.transfer(
-      this.Presale.address,
-      TOTAL_TOKENS,
-      {from:initialTokenHolder}
-    );
+    await this.Token.transfer(this.Presale.address, TOTAL_TOKENS, {
+      from: initialTokenHolder,
+    });
     await this.Presale.setTokenPools(
-        config.Presale.uniswapTokenBP,
-        config.Presale.presaleTokenBP,
-        [this.DaoLock.address,this.TeamLock.address,this.projectFund.address],
-        config.Presale.tokenDistributionBP
+      config.Presale.uniswapTokenBP,
+      config.Presale.presaleTokenBP,
+      [this.DaoLock.address, this.TeamLock.address, this.projectFund.address],
+      config.Presale.tokenDistributionBP
     );
-  })
+  });
 
-  describe("Stateless", function() {
+  describe("Stateless", function () {
+    describe("setWhitelist", async function () {
+      it("should revert for non-owner", async function () {
+        await expectRevert(
+          this.Presale.setWhitelist(depositors[0], true, {
+            from: depositors[0],
+          }),
+          "Ownable: caller is not the owner"
+        );
+      });
 
-  })
+      it("should whitelist non whitelisted account", async function () {
+        const whitelist = await this.Presale.whitelist(depositors[0]);
+        await this.Presale.setWhitelist(depositors[0], true, {
+          from: owner,
+        });
+        expect(whitelist).to.equal(false);
+        expect(await this.Presale.whitelist(depositors[0])).to.equal(true);
+      });
+      it("should unwhitelist account", async function () {
+        const whitelist = await this.Presale.whitelist(depositors[0]);
+        await this.Presale.setWhitelist(depositors[0], false, {
+          from: owner,
+        });
+        expect(whitelist).to.equal(true);
+        expect(await this.Presale.whitelist(depositors[0])).to.equal(false);
+      });
+    });
+  });
 
-  describe("State: Before Presale Start", function() {
+  describe("State: Before Presale Start", function () {
+    describe("#sendToUniswap", function () {
+      it("should revert", async function () {
+        await expectRevert(
+          this.Presale.sendToUniswap({ from: depositors[0] }),
+          "Presale not yet started."
+        );
+      });
+    });
+    describe("#deposit", function () {
+      it("should revert", async function () {
+        await expectRevert(
+          this.Presale.deposit({ from: depositors[0] }),
+          "Presale not yet started."
+        );
+      });
+    });
+  });
 
-  })
+  describe("State: Presale Active", function () {
+    it("should fail if less than 0.01 eth", async function () {
+      await expectRevert(
+        this.Presale.deposit({ from: depositors[0], value: "0" }),
+        "Must purchase at least 0.01 ether."
+      );
+    });
+  });
 
-  describe("State: Presale Active", function() {
+  describe("First depository succeed", function () {
+    before(async function () {
+      this.Presale.deposit({
+        from: depositors[0],
+        value: config.Presale.maxBuyPerAddress,
+      });
+    });
+  });
+  describe("Second depository succeed", function () {
+    before(async function () {
+      this.Presale.deposit({
+        from: depositors[1],
+        value: config.Presale.maxBuyPerAddress,
+      });
+    });
+  });
 
-  })
-
-  describe("State: Presale Ended", function() {
-
-  })
-})
+  describe("State: Presale Ended", function () {
+    describe("#deposit", function () {
+      it("should revert", async function () {
+        await expectRevert(
+          this.Presale.deposit({ from: depositors[0] }),
+          "Presale has ended."
+        );
+      });
+    });
+  });
+});
